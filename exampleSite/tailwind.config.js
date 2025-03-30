7ebb6fce50120239fd123aa2497a9aef4660d12f
Ram Iyer <2492952+theramiyer@users.noreply.github.com>
2025-03-30 20:16:23 +0530

    Update to using the new fonts

--- exampleSite/tailwind.config.js
+++ exampleSite/tailwind.config.js
@@ -5,7 +5,31 @@ module.exports = {
       "./themes/thenral-hugo/**/*.{html,js}"
     ],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            'button, input, select, textarea': {
+              fontWeight: '500',
+            },
+          }
+        },
+      },
+      fontFamily: {
+        sora: ['Sora', 'sans-serif'],
+        lora: ['Lora', 'serif'],
+      },
+      fontWeight: {
+        ui: 500,
+        uiSmall: 400,
+        heading: 700,
+        hero: 800,
+        body: 400,
+      },
+      letterSpacing: {
+        uppercase: '0.03em', // Add custom letter-spacing for uppercase
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
